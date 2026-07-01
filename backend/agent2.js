958882580d31b190a0d93d8858fb0b597ae6233c
Debottam Kar <debottamkar2003@gmail.com>
2026-07-01 14:01:42 +0530

    agent finding done

---
+++ backend/agent2.js
@@ -0,0 +1,15 @@
+import OpenAI from "openai";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new OpenAI({
+    apiKey: process.env.GROQ_API_KEY,
+    baseURL: "https://api.groq.com/openai/v1",
+});
+
+const response = await client.responses.create({
+    model: "openai/gpt-oss-20b",
+    input: "Explain the importance of cloud computing in modern software development. under 500 words",
+});
+console.log(response.output_text.replaceAll("**", "").replaceAll("##", "") || "");
